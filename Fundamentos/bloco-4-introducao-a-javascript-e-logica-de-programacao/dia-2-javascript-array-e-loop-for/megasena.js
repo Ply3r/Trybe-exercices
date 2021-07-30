1e1c4ed894303d2662beda75c81a913825ca3600
Leandro <Playertox1c@gmail.com>
2021-07-30 13:06:04 -0300

    adicionando melhorias no jogo

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/megasena.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/megasena.js
@@ -10,25 +10,14 @@ function gerarApostas(n) {
   let array = []
   let arrayAcertos = []
   let acertosTotais = 0
-  for(let c = 0; c < n; c++){
-    let play = []
-    for(let i = 0; i < 6; i++){
-      play.push(Math.floor(Math.random() * 60 + 1))
-    }
-    array.push(play)
-  }
+  for(let c = 0; c < n; c++){ array.push(gerarMegasena()) }
   array.forEach(e => {
     let acertoPlay = 0
-    for(let index of e){
-      megaSena.forEach(megaNumber => megaNumber == index ? ( 
-        acertosTotais++, 
-        acertoPlay++) : '')
-    }
+    for(let index of e){ megaSena.includes(index) ?(acertosTotais++, acertoPlay++) : '' }
     arrayAcertos.push(acertoPlay)
   })
   console.log(megaSena)
-  console.log(array)
   console.log(`Acertos Totais: ${acertosTotais}`)
-  array.forEach((e,i) => console.log(`Acertos Play ${i}: ${arrayAcertos[i]}`))
+  array.forEach((e,i) => arrayAcertos[i] == 6 ? console.log(`Acertos Play ${i}: ${arrayAcertos[i]}`) : '')
 }
-gerarApostas(3)
+gerarApostas(1000000)
