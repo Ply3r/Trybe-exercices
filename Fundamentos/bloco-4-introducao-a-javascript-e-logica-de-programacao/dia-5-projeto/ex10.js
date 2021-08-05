f7ef2db24c05545a198e0eb65650eeead600688f
Leandro <Playertox1c@gmail.com>
2021-08-05 16:42:34 -0300

    diminuindo tamanho do exercicio 10

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-5-projeto/ex10.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-5-projeto/ex10.js
@@ -2,9 +2,7 @@ function listaTech(arr, name) {
   let result = []
   arr.sort()
   arr.forEach(e => {
-    let obj = {}
-    obj.tech = e
-    obj.name = name
+    let obj = {tech: e, name: name}
     result.push(obj)
   })
   return result == '' ? 'Vazio!' : result
