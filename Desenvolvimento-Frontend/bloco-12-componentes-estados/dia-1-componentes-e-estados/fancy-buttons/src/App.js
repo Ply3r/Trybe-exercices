8e72539df8e68343c8da6453787d93e641f8bcef
Leandro <Playertox1c@gmail.com>
2021-09-13 15:51:50 -0300

    adicionando exercicios de fixaçao

--- Desenvolvimento-Frontend/bloco-12-componentes-estados/dia-1-componentes-e-estados/fancy-buttons/src/App.js
+++ Desenvolvimento-Frontend/bloco-12-componentes-estados/dia-1-componentes-e-estados/fancy-buttons/src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.css'
 
 class App extends Component {
-  handleClick() {
-    console.log('oiiiiiiiii')
+  constructor(props) {
+    super(props);
+    this.clicks1 = this.clicks1.bind(this);
+    this.clicks10 = this.clicks10.bind(this);
+    this.clicks100 = this.clicks100.bind(this);
+    this.state = {
+      clicks: 0
+    }
   }
 
+  clicks1() {this.setState((after, _props) => ({clicks: after.clicks += 1}))}
+  clicks10() {this.setState((after, _props) => ({clicks: after.clicks += 10}))}
+  clicks100() {this.setState((after, _props) => ({clicks: after.clicks += 100}))}
+
   render() {
-    return <button onClick={this.handleClick}>oi</button>
+    return (
+      <div>
+        <div className="container">{this.state.clicks}</div>
+        <button onClick={this.clicks1}>+1</button>
+        <button onClick={this.clicks10}>+10</button>
+        <button onClick={this.clicks100}>+100</button>
+      </div>
+    )
   }
 }
 
