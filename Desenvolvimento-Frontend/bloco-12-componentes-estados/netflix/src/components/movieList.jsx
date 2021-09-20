2534e59b45e4d3eb577a9552cd7e5e891a09fcdb
Leandro <play3rtox1c@gmail.com>
2021-09-20 18:20:30 -0300

    estilizando a netflix

--- Desenvolvimento-Frontend/bloco-12-componentes-estados/netflix/src/components/movieList.jsx
+++ Desenvolvimento-Frontend/bloco-12-componentes-estados/netflix/src/components/movieList.jsx
@@ -9,10 +9,10 @@ class MovieList extends Component {
     const { types, search } = this.props;
     return (
       <section>
-        <MovieSection types={types} search={search} title="Top rated:" page={Math.floor(Math.random() * 5)} />
-        <MovieSection types={types} search={search} title="You might like:" page={Math.floor(Math.random() * 5)} />
-        <MovieSection types={types} search={search} title="Others like'd:" page={Math.floor(Math.random() * 5)} />
-        <MovieSection types={types} search={search} title="More films:" page={Math.floor(Math.random() * 5)} />
+        <MovieSection types={types} search={search} title="Top rated:" page={Math.floor(Math.random() * 10)} />
+        <MovieSection types={types} search={search} title="You might like:" page={Math.floor(Math.random() * 10)} />
+        <MovieSection types={types} search={search} title="Others like'd:" page={Math.floor(Math.random() * 10)} />
+        <MovieSection types={types} search={search} title="More films:" page={Math.floor(Math.random() * 10)} />
       </section>
     );
   }
