8855240ae4f8d5c0dd0e8e1a6d15955bb1ed653d
Leandro <play3rtox1c@gmail.com>
2021-09-21 17:03:40 -0300

    adiciona bloco 13

---
+++ Desenvolvimento-Frontend/bloco-13-ciclo-de-vidas-de-componentes-react/dia-1-ciclo-de-vida-de-componentes/dad-jokes/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DadJokes from './dadJokes'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DadJokes />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
